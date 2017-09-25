9ef335c0a1a2e9b7c36646a7c2b8e029069a0b9d
Realbot <realbot@gmail.com>
2017-09-25 17:54:50 +0200

    Just sign-off
    Signed-off-by: Alberto Quario <realbot@gmail.com>

--- serve.go
+++ serve.go
@@ -32,7 +32,7 @@ func Serve(handlers *types.FaaSHandlers, config *types.FaaSConfig) {
 	r.HandleFunc("/function/{name:[-a-zA-Z_0-9]+}", handlers.FunctionProxy)
 	r.HandleFunc("/function/{name:[-a-zA-Z_0-9]+}/", handlers.FunctionProxy)
 	
-	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {fmt.Fprint(w, "OK")})
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "OK") })
 
 	readTimeout := config.ReadTimeout
 	writeTimeout := config.WriteTimeout
