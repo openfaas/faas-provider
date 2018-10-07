65b17de26f96c8491da28388008d6e32ecdd9ba5
Lucas Roesler <roesler.lucas@gmail.com>
2018-10-07 17:43:46 +0200

    Implement proxy httphandler factory
    **What**
    - Add standardized proxy handler based on the implementations for swarm
    and kubernetes
    
    **Why**
    - Reduce duplicated code amoung the various providers
    - Ensure the proxy behavior is consistent
    - Addresses openfaas/faas-provider#9
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

---
+++ handlers/proxy.go
@@ -0,0 +1,140 @@
+package handlers
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/openfaas/faas/gateway/requests"
+)
+
+const (
+	watchdogPort       = 8080
+	defaultContentType = "text/plain"
+)
+
+// MakeProxyHandler creates a standard http HandlerFunc to proxy function requests to the function.
+// The FaaS provider implementation is responsible for providing the resolver function implementation.
+// resolver will receive the function name and should return the address of the function service.
+func MakeProxyHandler(timeout time.Duration, resolver func(string) string) http.HandlerFunc {
+	proxyClient := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   timeout,
+				KeepAlive: 1 * time.Second,
+			}).DialContext,
+			IdleConnTimeout:       120 * time.Millisecond,
+			ExpectContinueTimeout: 1500 * time.Millisecond,
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Body != nil {
+			defer r.Body.Close()
+		}
+
+		switch r.Method {
+		case http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+			http.MethodGet:
+
+			proxyRequest(w, r, proxyClient, resolver)
+
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+// proxyRequest handles the actual resolution of and then request to the function service.
+func proxyRequest(w http.ResponseWriter, originalReq *http.Request, proxyClient http.Client, resolver func(string) string) {
+
+	functionName := getFunctionName(originalReq)
+	if functionName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Provide an x-function header or valid route /function/function_name."))
+		return
+	}
+
+	functionAddr := resolver(functionName)
+	if functionAddr == "" {
+		// TODO: Should record the 404/not found error in Prometheus.
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("Cannot find service: %s.", functionName)))
+		return
+	}
+
+	defer func(when time.Time) {
+		seconds := time.Since(when).Seconds()
+		log.Printf("%s took %f seconds\n", functionName, seconds)
+	}(time.Now())
+
+	forwardReq := requests.NewForwardRequest(originalReq.Method, *originalReq.URL)
+	url := forwardReq.ToURL(functionAddr, watchdogPort)
+	proxyReq, _ := http.NewRequest(originalReq.Method, url, originalReq.Body)
+	defer proxyReq.Body.Close()
+
+	copyHeaders(&proxyReq.Header, &originalReq.Header)
+
+	response, err := proxyClient.Do(proxyReq)
+	if err != nil {
+		log.Println("[ERROR]", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		buf := bytes.NewBufferString("Can't reach service for: " + functionName)
+		w.Write(buf.Bytes())
+		return
+	}
+
+	clientHeader := w.Header()
+	copyHeaders(&clientHeader, &response.Header)
+	w.Header().Set("Content-Type", getContentType(response.Header, originalReq.Header))
+
+	w.WriteHeader(http.StatusOK)
+	io.Copy(w, response.Body)
+}
+
+// getFunctionName inspects the request and returns the specified function.  This allows the
+// caller to specify the function via the X-Function header or as part to the URL path.  The
+// name found in the header is ignored if a name can be found in the URL path.
+func getFunctionName(r *http.Request) (functionName string) {
+	functionName = mux.Vars(r)["name"]
+	if functionName == "" {
+		functionName = r.Header.Get("X-Function")
+		log.Print("trying X-Function header, found: ", functionName)
+	}
+
+	return functionName
+}
+
+// copyHeaders clones the header values from the source into the destination.
+func copyHeaders(destination *http.Header, source *http.Header) {
+	for k, v := range *source {
+		vClone := make([]string, len(v))
+		copy(vClone, v)
+		(*destination)[k] = vClone
+	}
+}
+
+// getContentType resolves the correct Content-Tyoe for a proxied function.
+func getContentType(request http.Header, proxyResponse http.Header) (headerContentType string) {
+	responseHeader := proxyResponse.Get("Content-Type")
+	requestHeader := request.Get("Content-Type")
+
+	if len(responseHeader) > 0 {
+		headerContentType = responseHeader
+	} else if len(requestHeader) > 0 {
+		headerContentType = requestHeader
+	} else {
+		headerContentType = defaultContentType
+	}
+
+	return headerContentType
+}
