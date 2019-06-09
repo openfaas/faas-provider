2859f162e84e6cdd7ec8605473e39134a701a7f9
Keiran Smith <contact@keiran.scot>
2019-06-09 21:58:12 +0100

    Add support for Multiple Users in Basic Auth
    Signed-off-by: Keiran Smith <contact@keiran.scot>

--- auth/basic_auth.go
+++ auth/basic_auth.go
@@ -15,12 +15,39 @@ func DecorateWithBasicAuth(next http.HandlerFunc, credentials *BasicAuthCredenti
 		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
 
 		if !ok || !(credentials.Password == password && user == credentials.User) {
-
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("invalid credentials"))
-			return
+			unauthorized(w, r)
 		}
 
 		next.ServeHTTP(w, r)
 	}
+}
+
+// Support for Multiple User Basic Auth Providers
+func DecorateWithMultiUserBasicAuth(next http.HandlerFunc, creds []BasicAuthCredentials) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		ok := false
+		user, password, _ := r.BasicAuth()
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+		for _, v := range creds {
+			if v.User == user && v.Password == password {
+				ok = true
+				break
+			}
+		}
+
+		if !ok {
+			unauthorized(w, r)
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+// Helper function to return a 401
+func unauthorized(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte("invalid credentials"))
+	return
 }
--- auth/basic_auth_test.go
+++ auth/basic_auth_test.go
@@ -64,3 +64,92 @@ func Test_AuthWithInvalidPassword_Gives403(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func Test_MultiUserAuthWithValidPassword_Gives200(t *testing.T) {
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	}
+	w := httptest.NewRecorder()
+
+	wantUser := "admin"
+	wantPassword := "password"
+	r := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	r.SetBasicAuth(wantUser, wantPassword)
+	wantCredentials := getMultiUserAuthCreds()
+
+	decorated := DecorateWithMultiUserBasicAuth(handler, wantCredentials)
+	decorated.ServeHTTP(w, r)
+
+	wantCode := http.StatusOK
+
+	if w.Code != wantCode {
+		t.Errorf("status code, want: %d, got: %d", wantCode, w.Code)
+		t.Fail()
+	}
+}
+
+func Test_MultiUserAuthWithInvalidPassword_Gives401(t *testing.T) {
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	}
+	w := httptest.NewRecorder()
+
+	wantUser := "admin"
+	wantPassword := "apassword"
+	r := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	r.SetBasicAuth(wantUser, wantPassword)
+	wantCredentials := getMultiUserAuthCreds()
+
+	decorated := DecorateWithMultiUserBasicAuth(handler, wantCredentials)
+	decorated.ServeHTTP(w, r)
+
+	wantCode := http.StatusUnauthorized
+
+	if w.Code != wantCode {
+		t.Errorf("status code, want: %d, got: %d", wantCode, w.Code)
+		t.Fail()
+	}
+}
+
+func Test_MultiUserAuthWithInvalidUser_Gives401(t *testing.T) {
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	}
+	w := httptest.NewRecorder()
+
+	wantUser := "affix"
+	wantPassword := "apassword"
+	r := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	r.SetBasicAuth(wantUser, wantPassword)
+	wantCredentials := getMultiUserAuthCreds()
+
+	decorated := DecorateWithMultiUserBasicAuth(handler, wantCredentials)
+	decorated.ServeHTTP(w, r)
+
+	wantCode := http.StatusUnauthorized
+
+	if w.Code != wantCode {
+		t.Errorf("status code, want: %d, got: %d", wantCode, w.Code)
+		t.Fail()
+	}
+}
+
+func getMultiUserAuthCreds() []BasicAuthCredentials {
+	return []BasicAuthCredentials{
+		{
+			User:     "admin",
+			Password: "password",
+		},
+		{
+			User:     "auser",
+			Password: "apassword",
+		},
+		{
+			User:     "someoneelse",
+			Password: "anotherpass",
+		},
+	}
+}
