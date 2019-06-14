7ad4f3718438bbc82e52c28b77a42003d0ad28fc
stefanprodan <stefan.prodan@gmail.com>
2019-06-14 11:34:14 +0300

    Add environment reader to bootstrap/v2 Ported from github.com/openfaas/faas-swarm
    Signed-off-by: stefanprodan <stefan.prodan@gmail.com>

---
+++ bootstrap/v2/env.go
@@ -0,0 +1,62 @@
+// Copyright 2019 OpenFaaS Authors
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package v2
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+// EnvReader wraps os.Getenv and allows fallback values for not found environment variables
+type EnvReader struct {
+}
+
+func (*EnvReader) ParseString(key string, fallback string) string {
+	val := os.Getenv(key)
+	if len(val) > 0 {
+		return val
+	}
+	return fallback
+}
+
+func (*EnvReader) ParseInt(key string, fallback int) int {
+	val := os.Getenv(key)
+	if len(val) > 0 {
+		parsedVal, parseErr := strconv.Atoi(val)
+		if parseErr == nil && parsedVal >= 0 {
+			return parsedVal
+		}
+	}
+	return fallback
+}
+
+func (*EnvReader) ParseIntOrDuration(key string, fallback time.Duration) time.Duration {
+	val := os.Getenv(key)
+	if len(val) > 0 {
+		parsedVal, parseErr := strconv.Atoi(val)
+		if parseErr == nil && parsedVal >= 0 {
+			return time.Duration(parsedVal) * time.Second
+		}
+	}
+
+	duration, durationErr := time.ParseDuration(val)
+	if durationErr != nil {
+		return fallback
+	}
+
+	return duration
+}
+
+func (*EnvReader) ParseBool(key string, fallback bool) bool {
+	val := os.Getenv(key)
+	switch val {
+	case "1", "true":
+		return true
+	case "0", "false":
+		return false
+	default:
+		return fallback
+	}
+}
